547fb6d76692286d580be7bd0b952b43c1c8f04c
purpleeeee <h.purpleeeee@gmail.com>
2021-07-20 20:42:11 +0900

    feat: domain

---
+++ src/domain/pokemon/pokemon.ts
@@ -0,0 +1,40 @@
+import { NamedAPIResource } from "./types";
+
+export type Pokemon = {
+  id: number;
+  name: string;
+  baseExperience: number;
+  height: number;
+  isDefault: boolean;
+  order: number;
+  weight: number;
+  abilities: {
+    isHidden: boolean;
+    slot: number;
+    ability: NamedAPIResource;
+  }[];
+  forms: NamedAPIResource[];
+  moves: {
+    move: NamedAPIResource;
+  }[];
+  sprites: {
+    frontDefault: string;
+    frontShiny: string;
+    frontFemale: string;
+    frontShinyFemale: string;
+    backDefault: string;
+    backShiny: string;
+    backFemale: string;
+    backShinyFemale: string;
+  };
+  species: NamedAPIResource[];
+  stats: {
+    baseStat: number;
+    effort: number;
+    stat: NamedAPIResource;
+  }[];
+  types: {
+    slot: number;
+    type: NamedAPIResource;
+  }[];
+};
